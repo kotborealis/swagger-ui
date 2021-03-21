c633728730062a65ed09f08a9617809fce1f3628
kotborealis <kotborealis@awooo.ru>
2021-03-21 13:49:49 +0300

    Added rust-embed to pack swagger-ui assets into binary

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,48 @@
+use rust_embed::RustEmbed;
+
+#[derive(RustEmbed)]
+#[folder = "$SWAGGER_UI_DIST_PATH"]
+pub struct SwaggerUiAssets;
+
 #[cfg(test)]
 mod tests {
+    use std::path::Path;
+    use crate::SwaggerUiAssets;
+
+    fn asset_list() -> [&'static str; 8] {
+        [
+            "favicon-16x16.png",
+            "favicon-32x32.png",
+            "index.html",
+            "oauth2-redirect.html",
+            "swagger-ui.css",
+            "swagger-ui.js",
+            "swagger-ui-bundle.js",
+            "swagger-ui-standalone-preset.js",
+        ]
+    }
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn swagger_ui_dist_exists() {
+        let dist = env!("SWAGGER_UI_DIST_PATH");
+        println!("Checking if dist env var is set ({})", dist);
+        assert!(!dist.is_empty());
+
+        println!("Checking if assets exists");
+        for file in &asset_list() {
+            let asset = format!("{}/{}", dist, file);
+            println!("\t{}", asset);
+            assert!(Path::new(&asset).exists());
+        }
+    }
+
+    #[test]
+    fn swagger_ui_assets() {
+        println!("Checking if assets exists in binary");
+        for asset in &asset_list() {
+            println!("\t{}", asset);
+            let data = SwaggerUiAssets::get(&asset).unwrap();
+            assert!(!data.is_empty());
+        }
     }
 }
